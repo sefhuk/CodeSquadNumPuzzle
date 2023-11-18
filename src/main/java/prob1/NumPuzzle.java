65d8424b5a59855efe6ea784c9482d1e92dde6a8
sefhuk <ish0411@naver.com>
2023-11-18 21:40:19 +0900

    Feat: 사용자로부터 2개의 숫자를 입력받는 기능 구현

---
+++ src/main/java/prob1/NumPuzzle.java
@@ -0,0 +1,19 @@
+package main.java.prob1;
+
+import java.util.*;
+
+public class NumPuzzle {
+
+    public static String getInput() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("교환할 두 숫자를 입력>");
+        return sc.nextLine();
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
