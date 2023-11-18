a16a09c4d200ba39cde479de68e70f0ce1ac3c5f
sefhuk <ish0411@naver.com>
2023-11-18 21:42:57 +0900

    Feat: 사용자가 입력한 값이 유요한지 검사하는 기능 구현
     - checkSpaceRule : 입력받은 문자열의 공백 규칙 확인.
     - checkNumericRule : 입력받은 문자열의 각 토큰이 정수인지 확인.
     - checkRangeRule : 각 정수 토큰의 범위 규칙 확인.

--- src/main/java/prob1/NumPuzzle.java
+++ src/main/java/prob1/NumPuzzle.java
@@ -12,6 +12,61 @@ public class NumPuzzle {
         return sc.nextLine();
     }
 
+    public static boolean checkSpaceRule(String token, int index) {
+
+        if (index == 0) {
+            if (token.charAt(0) == ' ' || token.charAt(token.length() - 1) == ' ') {
+                return false;
+            }
+        } else {
+            if (token.length() > 1) {
+                if (token.charAt(0) == ' ' && token.charAt(1) == ' ') {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean checkNumericRule(String token) {
+
+        try {
+            Integer.parseInt(token);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+    }
+
+    public static boolean checkRangeRule(int number) {
+
+        if (number < 1 || number > 8) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isInputValid(String input) {
+
+        String[] tokens = input.split(",");
+        if (tokens.length != 2) {
+            return false;
+        }
+
+        for (int i = 0; i < 2; i++) {
+            String token = tokens[i];
+            if (!checkSpaceRule(token, i) || !checkNumericRule(token.trim())
+                    || !checkRangeRule(Integer.parseInt(token.trim()))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
 
 
