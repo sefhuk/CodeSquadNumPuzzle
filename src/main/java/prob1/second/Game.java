b8338aed39b1ae9ea407b85d7f59405dd8f6cd7a
sefhuk <ish0411@naver.com>
2023-11-22 20:47:32 +0900

    Feat: 사용자의 입력이 문제가 없는지 확인하는 기능 구현
     - checkNumericRule : 정수를 입력했는지 확인함.
     - isMovable : 입력된 정수의 퍼즐이 이동이 가능한 번호인지 확인함.

--- src/main/java/prob1/second/Game.java
+++ src/main/java/prob1/second/Game.java
@@ -1,5 +1,6 @@
 package prob1.second;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Game {
@@ -15,5 +16,47 @@ public class Game {
         System.out.print("숫자 입력> ");
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
+    }
+
+    /**
+     * 사용자가 입력한 값이 정수이며, 정상 범위인지 확인하는 함수
+     * 정상이면 정수로 변환하여 반환하고, 비정상이면 0을 반환함.
+     *
+     * @param input 사용자가 입력한 문자열
+     * @return 정상 여부에 따른 정수값
+     */
+    public static int checkNumericRule(String input) {
+
+        try {
+            int num = Integer.parseInt(input);
+            return num;
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    /**
+     * 해당 번호 퍼즐이 이동할 수 있는지 확인하는 함수
+     *
+     * @param puzzle 현재 퍼즐 List
+     * @param num 사용자가 입력한 퍼즐 번호
+     * @return 이동 가능 여부
+     */
+    public static boolean isMovable(List<Integer> puzzle, int num) {
+
+        int numIndex = puzzle.indexOf(num); // 퍼즐 번호의 인덱스
+        int[] targetIndex = {numIndex + 4, numIndex - 4, numIndex + 1, numIndex - 1};
+
+        for (int i : targetIndex) {
+            if (i < 0 || i > 15) {
+                continue;
+            }
+
+            if (puzzle.get(i) == 16) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
