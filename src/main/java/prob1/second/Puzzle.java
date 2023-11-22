2bea74a461644d0ce3d3dcfc5fb65b289b497e52
sefhuk <ish0411@naver.com>
2023-11-22 20:40:15 +0900

    Feat: 게임을 위한 퍼즐 클래스 및 생성자 구현
     - 생성자를 호출하면 랜덤 퍼즐을 생성해 멤버변수 puzzle에 할당함.

---
+++ src/main/java/prob1/second/Puzzle.java
@@ -0,0 +1,46 @@
+package prob1.second;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+/**
+ * 게임에서 사용할 퍼즐 클래스
+ */
+public class Puzzle {
+
+    private List<Integer> puzzle; // 퍼즐 List
+
+    /**
+     * 퍼즐 생성자
+     * 퍼즐을 섞어서 멤버 puzzle에 세팅한다.
+     *
+     * @param mixCount 섞을 횟수
+     */
+    Puzzle(int mixCount) {
+
+        List<Integer> puzzle = new ArrayList<>();
+
+        for (int i = 0; i < 16; i++) {
+            puzzle.add(i + 1); // 퍼즐 빈 공간은 16으로 세팅
+        }
+
+        Random random = new Random();
+
+        for (int i = 0; i < mixCount; i++) {
+
+            // 1~15 인덱스 중 랜덤하게 두 값을 뽑기
+            int start = random.nextInt(7) + 1; // 1 ~ 7
+            int end = random.nextInt(8) + 8; // 8 ~ 15
+
+
+            // 뽑힌 두 인덱스 사이의 값들을 퍼즐의 맨 앞에 추가
+            List<Integer> sub = new ArrayList(puzzle.subList(start, end + 1));
+            puzzle.removeAll(sub);
+            puzzle.addAll(0, sub);
+        }
+
+        this.puzzle = puzzle;
+    }
+}
